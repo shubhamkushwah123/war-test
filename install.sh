96003effee9af5cb738d5aaed8d250a57120cc6b
Ubuntu <ubuntu@ip-172-31-13-36.us-east-2.compute.internal>
2024-02-22 07:34:35 +0000

    install.sh updated

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 aws s3 cp s3://aws-code-deploy-bucket-shubhamkushwah123/test-war/war-test.war /tmp
-sudo mv /tmp/war-test.war /usr/share/tomcat/webapps/war-test.war
-sudo service tomcat restart
+sudo mv /tmp/war-test.war /usr/share/tomcat9/webapps/war-test.war
+sudo service tomcat9 restart
