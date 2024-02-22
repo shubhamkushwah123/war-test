fbcd842f000b4e80d3366fd121b998482e3f8436
Ubuntu <ubuntu@ip-172-31-13-36.us-east-2.compute.internal>
2024-02-22 06:52:51 +0000

    yml configuration

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://aws-code-deploy-bucket-shubhamkushwah123/target/war-test.war /tmp
+aws s3 cp s3://aws-code-deploy-bucket-shubhamkushwah123/test-war/war-test.war /tmp
 sudo mv /tmp/war-test.war /usr/share/tomcat/webapps/war-test.war
 sudo service tomcat restart
